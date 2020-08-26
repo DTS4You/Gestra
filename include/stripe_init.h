302d036ccc23e864b25c3df83690db4511db4453
Norbert Schwarz <Norbert.Schwarz@WDE20DTS01.datentechnikschwarz.local>
2020-08-26 18:54:09 +0200

    Init

--- include/stripe_init.h
+++ include/stripe_init.h
@@ -11,35 +11,35 @@
 
 #define STRIPE_1_DDB        0
 #define STRIPE_1_START      0
-#define STRIPE_1_NUMBER     59  
+#define STRIPE_1_NUMBER     64  
 
 #define STRIPE_2_DDB        0
 #define STRIPE_2_START      STRIPE_1_NUMBER
-#define STRIPE_2_NUMBER     34          // Geo-SAT zu Cluster-SAT
+#define STRIPE_2_NUMBER     64
 
-#define STRIPE_3_DDB        1
-#define STRIPE_3_START      0
-#define STRIPE_3_NUMBER     29          // Bodenstation 1. Cluster-SAT
+#define STRIPE_3_DDB        0
+#define STRIPE_3_START      ( STRIPE_1_NUMBER + STRIPE_2_NUMBER )
+#define STRIPE_3_NUMBER     64          
 
-#define STRIPE_4_DDB        1
-#define STRIPE_4_START      STRIPE_3_NUMBER
-#define STRIPE_4_NUMBER     15          // 1. Cluster-SAT -> 2. Cluster-SAT
+#define STRIPE_4_DDB        0
+#define STRIPE_4_START      ( STRIPE_1_NUMBER + STRIPE_2_NUMBER + STRIPE_3_NUMBER )
+#define STRIPE_4_NUMBER     64          
 
 #define STRIPE_5_DDB        1
 #define STRIPE_5_START      ( STRIPE_4_START + STRIPE_4_NUMBER )
-#define STRIPE_5_NUMBER     15          // 2. Cluster-SAT -> 3. Cluster-SAT
+#define STRIPE_5_NUMBER     15          
 
 #define STRIPE_6_DDB        1
 #define STRIPE_6_START      ( STRIPE_5_START + STRIPE_5_NUMBER )
-#define STRIPE_6_NUMBER     15          // 3. Cluster-SAT -> 4. Cluster-SAT
+#define STRIPE_6_NUMBER     15          
 
 #define STRIPE_7_DDB        1
 #define STRIPE_7_START      ( STRIPE_6_START + STRIPE_6_NUMBER )
-#define STRIPE_7_NUMBER     15          // 4. Cluster-SAT -> 1. Cluster-SAT
+#define STRIPE_7_NUMBER     15          
 
 #define STRIPE_8_DDB        1
 #define STRIPE_8_START      ( STRIPE_7_START + STRIPE_7_NUMBER )
-#define STRIPE_8_NUMBER     20          // 1. Cluster-SAT -> 3. Cluster-SAT diagonal
+#define STRIPE_8_NUMBER     20          
 
 #define STRIPE_9_DDB        1
 #define STRIPE_9_START      ( STRIPE_8_START + STRIPE_8_NUMBER )
