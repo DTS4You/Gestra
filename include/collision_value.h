b490b03b4a3b25f25aec104a6fa9a149ecc79495
Norbert Schwarz <Norbert.Schwarz@WDE20DTS01.datentechnikschwarz.local>
2020-08-28 15:26:30 +0200

    Init

--- include/collision_value.h
+++ include/collision_value.h
@@ -9,3 +9,29 @@
 // Kollisionswerte
 #define TRACK_01_COL    23
 #define TRACK_02_COL    23
+#define TRACK_03_COL    23
+#define TRACK_04_COL    23
+#define TRACK_05_COL    23
+#define TRACK_06_COL    23
+#define TRACK_07_COL    23
+#define TRACK_08_COL    23
+#define TRACK_10_COL    23
+#define TRACK_11_COL    23
+#define TRACK_12_COL    23
+#define TRACK_13_COL    23
+#define TRACK_14_COL    23
+#define TRACK_15_COL    23
+#define TRACK_16_COL    23
+#define TRACK_17_COL    23
+#define TRACK_18_COL    23
+#define TRACK_19_COL    23
+#define TRACK_20_COL    23
+#define TRACK_21_COL    23
+#define TRACK_22_COL    23
+#define TRACK_23_COL    23
+#define TRACK_24_COL    23
+
+#define RADAR_01_COL    13
+#define RADAR_02_COL    13
+#define RADAR_03_COL    13
+#define RADAR_04_COL    13
